f3eee35cbb29b6f220094b2ff841aeeab7709a30
HPE26 <1210929829@qq.com>
2024-12-04 17:23:07 +0800

    Add files via upload

---
+++ MSFNet/lib/core/loss.py
@@ -0,0 +1,128 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Bin Xiao (Bin.Xiao@microsoft.com)
+# ------------------------------------------------------------------------------
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import math
+
+
+class JointsMSELoss(nn.Module):
+    def __init__(self, use_target_weight, edge_weight=1):
+        super(JointsMSELoss, self).__init__()
+        self.criterion = nn.MSELoss(reduction='mean')
+        self.use_target_weight = use_target_weight
+        self.edge_weight = edge_weight  
+
+    def forward(self, output, target, target_weight):
+        batch_size = output.size(0)
+        num_joints = output.size(1)
+        heatmaps_pred = output.reshape((batch_size, num_joints, -1)).split(1, 1)
+        heatmaps_gt = target.reshape((batch_size, num_joints, -1)).split(1, 1)
+        loss = 0
+
+        for idx in range(num_joints):
+            heatmap_pred = heatmaps_pred[idx].squeeze()
+            heatmap_gt = heatmaps_gt[idx].squeeze()
+            if self.use_target_weight:
+                loss += 0.5 * self.criterion(
+                    heatmap_pred.mul(target_weight[:, idx]),
+                    heatmap_gt.mul(target_weight[:, idx])
+                )
+            else:
+                loss += 0.5 * self.criterion(heatmap_pred, heatmap_gt)
+
+        edge_loss = 0
+        for idx in range(num_joints):
+            heatmap_pred = heatmaps_pred[idx].squeeze()
+            heatmap_gt = heatmaps_gt[idx].squeeze()
+        
+            edge_mask = self.generate_edge_mask(heatmap_pred)
+            if self.use_target_weight:
+                edge_loss += self.criterion(
+                    heatmap_pred.mul(target_weight[:, idx]).mul(edge_mask),
+                    heatmap_gt.mul(target_weight[:, idx]).mul(edge_mask)
+                )
+            else:
+                edge_loss += self.criterion(
+                    heatmap_pred.mul(edge_mask),
+                    heatmap_gt.mul(edge_mask)
+                )
+
+        total_loss = (loss / num_joints) + self.edge_weight * (edge_loss / num_joints)
+        return total_loss
+
+    def generate_edge_mask(self, heatmap):
+ 
+        mask = torch.zeros_like(heatmap)
+
+   
+        edge_size = 1 
+
+
+        if heatmap.dim() == 2:
+ 
+            mask[:, edge_size:-edge_size] = 1
+        elif heatmap.dim() == 3:
+ 
+            batch_size, channels, height, width = heatmap.size()
+            for i in range(batch_size):
+                for j in range(channels):
+                    mask[i, j, edge_size:-height+edge_size, edge_size:-width+edge_size] = 1
+        else:
+            raise ValueError("Unsupported dimension for heatmap")
+
+        return mask
+
+
+
+class JointsOHKMMSELoss(nn.Module):
+    def __init__(self, use_target_weight, topk=8):
+        super(JointsOHKMMSELoss, self).__init__()
+        self.criterion = nn.MSELoss(reduction='none')
+        self.use_target_weight = use_target_weight
+        self.topk = topk
+
+    def ohkm(self, loss):
+        ohkm_loss = 0.
+        for i in range(loss.size()[0]):
+            sub_loss = loss[i]
+            topk_val, topk_idx = torch.topk(
+                sub_loss, k=self.topk, dim=0, sorted=False
+            )
+            tmp_loss = torch.gather(sub_loss, 0, topk_idx)
+            ohkm_loss += torch.sum(tmp_loss) / self.topk
+        ohkm_loss /= loss.size()[0]
+        return ohkm_loss
+
+    def forward(self, output, target, target_weight):
+        batch_size = output.size(0)
+        num_joints = output.size(1)
+        heatmaps_pred = output.reshape((batch_size, num_joints, -1)).split(1, 1)
+        heatmaps_gt = target.reshape((batch_size, num_joints, -1)).split(1, 1)
+
+        loss = []
+        for idx in range(num_joints):
+            heatmap_pred = heatmaps_pred[idx].squeeze()
+            heatmap_gt = heatmaps_gt[idx].squeeze()
+            if self.use_target_weight:
+                loss.append(0.5 * self.criterion(
+                    heatmap_pred.mul(target_weight[:, idx]),
+                    heatmap_gt.mul(target_weight[:, idx])
+                ))
+            else:
+                loss.append(
+                    0.5 * self.criterion(heatmap_pred, heatmap_gt)
+                )
+
+        loss = [l.mean(dim=1).unsqueeze(dim=1) for l in loss]
+        loss = torch.cat(loss, dim=1)
+
+        return self.ohkm(loss)
+
---
+++ MSFNet/lib/models/resample.py
@@ -0,0 +1,99 @@
+# from ..NN import common
+
+import torch.nn as nn
+import math
+
+def make_model(args, parent=False):
+    return Resample(args)
+
+
+def default_conv(in_channels, out_channels, kernel_size, bias=True):
+    return nn.Conv2d(
+        in_channels, out_channels, kernel_size,
+        padding=(kernel_size // 2), bias=bias)
+
+
+class Upsampler(nn.Sequential):
+    def __init__(self, conv, scale, n_feat, bn=False, act=False, bias=True):
+        m = []
+        if (scale & (scale - 1)) == 0:  # Is scale = 2^n?
+            for _ in range(int(math.log(scale, 2))):
+                m.append(conv(n_feat, 4 * n_feat, 3, bias))
+                m.append(nn.PixelShuffle(2))
+                if bn: m.append(nn.BatchNorm2d(n_feat))
+                if act: m.append(act())
+        elif scale == 3:
+            m.append(conv(n_feat, 9 * n_feat, 3, bias))
+            m.append(nn.PixelShuffle(3))
+            if bn: m.append(nn.BatchNorm2d(n_feat))
+            if act: m.append(act())
+        else:
+            raise NotImplementedError
+
+        super(Upsampler, self).__init__(*m)
+
+
+class ResBlock(nn.Module):
+    def __init__(self, conv, n_feat, kernel_size, bias=True, bn=False, act=nn.ReLU(True), res_scale=1):
+        super(ResBlock, self).__init__()
+        m = []
+        for i in range(2):
+            m.append(conv(n_feat, n_feat, kernel_size, bias=bias))
+            if bn: m.append(nn.BatchNorm2d(n_feat))
+            if i == 0: m.append(act)
+        self.body = nn.Sequential(*m)
+        self.res_scale = res_scale
+
+    def forward(self, x):
+        res = self.body(x).mul(self.res_scale)
+        res += x
+        return res
+
+
+class Resample(nn.Module):
+    def __init__(self, num_channels=3, input_channel=64, factor=4, width=64, depth=16, kernel_size=3,
+                 conv=default_conv):
+        super(Resample, self).__init__()
+
+        n_resblock = depth
+        n_feats = width
+        kernel_size = kernel_size
+        scale = factor
+        act = nn.ReLU()
+
+        m_head = [conv(input_channel, n_feats, kernel_size)]
+
+        m_body = [ResBlock(conv, n_feats, kernel_size, act=act, res_scale=1.) for _ in range(n_resblock)]
+        m_body.append(conv(n_feats, n_feats, kernel_size))
+
+        m_tail = [Upsampler(conv, scale, n_feats, act=False), conv(n_feats, num_channels, kernel_size)]
+
+        self.head = nn.Sequential(*m_head)
+        self.body = nn.Sequential(*m_body)
+        self.tail = nn.Sequential(*m_tail)
+
+    def forward(self, x):
+        x = self.head(x)
+        res = self.body(x)
+        res += x
+        x = self.tail(res)
+        return x
+
+    def load_state_dict(self, state_dict, strict=True):
+        own_state = self.state_dict()
+        for name, param in state_dict.items():
+            if name in own_state:
+                if isinstance(param, nn.Parameter):
+                    param = param.data
+                try:
+                    own_state[name].copy_(param)
+                except Exception:
+                    if name.find('tail') == -1:
+                        raise RuntimeError('While copying the parameter named {}, '
+                                           'whose dimensions in the model are {} and '
+                                           'whose dimensions in the checkpoint are {}.'
+                                           .format(name, own_state[name].size(), param.size()))
+            elif strict:
+                if name.find('tail') == -1:
+                    raise KeyError('unexpected key "{}" in state_dict'
+                                   .format(name))
